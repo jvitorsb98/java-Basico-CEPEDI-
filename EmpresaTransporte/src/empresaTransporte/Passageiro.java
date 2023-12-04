11e28df2b309a96c19af045b939b748b29368118
João Ramos <jvitorsb98@outlook.com>
2023-12-04 11:38:00 -0300

    Atividade java individual

---
+++ EmpresaTransporte/src/empresaTransporte/Passageiro.java
@@ -0,0 +1,16 @@
+package empresaTransporte;
+
+public class Passageiro extends Pessoa {
+	
+	private static int numeroDePasageiros;
+	
+	private int id;
+	
+	private CartaoPassagem cartao;
+
+	public Passageiro(String nome, String cpf, int idade, String rG, CartaoPassagem cartao) {
+		super(nome, cpf, idade, rG);
+		this.cartao = cartao;
+	}
+	
+}
---
+++ EmpresaTransporte/src/empresaTransporte/Verificacoes.java
@@ -0,0 +1,25 @@
+package empresaTransporte;
+
+public abstract class Verificacoes {
+	
+	//realiza verificação do cpf conforme as normas para isso
+	public static boolean verificaCPF() {
+		
+	}
+	
+	//realiza verificação da placa conforme as normas para isso
+	public static boolean verificaPlaca() {
+		
+	}
+	//realiza verificação do rg conforme as normas para isso
+	public static boolean verificaRG() {
+		
+	}
+	//realiza verificação do renavam conforme as normas para isso
+	public static boolean verificaRenavam() {
+		
+	}
+	
+	
+	
+}
